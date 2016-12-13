48d4bb5390af7a1c230b7cf7acf3fb4f080ea3ac
Sebastian Hoß <mail@shoss.de>
2016-12-13 10:34:46 +0100

    add Gson converter
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ thats-interesting-converters/thats-interesting-gson/src/main/java/de/xn__ho_hia/interesting/converter/GsonConverter.java
@@ -0,0 +1,54 @@
+package de.xn__ho_hia.interesting.converter;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.function.Supplier;
+
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonWriter;
+
+/**
+ * A converter that formats a method invocation using a GSON {@link ObjectMapper}.
+ */
+public final class GsonConverter implements InvocationConverter<String> {
+
+    private final Gson gson;
+
+    /**
+     * @param gson
+     *            The Gson instance to use.
+     */
+    public GsonConverter(final Gson gson) {
+        this.gson = gson;
+    }
+
+    @Override
+    public String convert(final Object proxy, final Method method, final Object[] args,
+            final Map<String, Supplier<Object>> extras) {
+    	final StringWriter writer = new StringWriter();
+		try {
+			final Parameter[] parameters = method.getParameters();
+			JsonWriter jsonWriter = gson.newJsonWriter(writer)
+				.beginObject();
+
+	        for (int index = 0; index < args.length; index++) {
+	        	jsonWriter.name(parameters[index].getName());
+	        	jsonWriter.jsonValue(gson.toJson(args[index]));
+	        }
+	        for (final Entry<String, Supplier<Object>> entry : extras.entrySet()) {
+	        	jsonWriter.name(entry.getKey());
+	        	jsonWriter.jsonValue(gson.toJson(entry.getValue().get()));
+	        }
+			
+	        jsonWriter.endObject().close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    	return writer.toString();
+    }
+
+}
---
+++ thats-interesting-converters/thats-interesting-gson/src/main/java/de/xn__ho_hia/interesting/converter/JacksonConverters.java
@@ -0,0 +1,26 @@
+package de.xn__ho_hia.interesting.converter;
+
+import com.google.gson.Gson;
+
+/**
+ * Factory for Gson based converters.
+ */
+public final class JacksonConverters {
+
+    /**
+     * @return The configured method invocation converter.
+     */
+    public static final InvocationConverter<String> json() {
+        return json(new Gson());
+    }
+
+    /**
+     * @param gson
+     *            The Gson instance to use.
+     * @return The configured method invocation converter.
+     */
+    public static final InvocationConverter<String> json(final Gson gson) {
+        return new GsonConverter(gson);
+    }
+
+}
