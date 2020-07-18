3c7e229b606301d076f90bb6b008b57ee9a5389e
Sebastian Hoß <sebhoss@pm.me>
2020-07-18 16:49:57 +0200

    add copyright headers

--- thats-interesting-core/src/main/java/wtf/metio/ti/handler/package-info.java
+++ thats-interesting-core/src/main/java/wtf/metio/ti/handler/package-info.java
@@ -1,3 +1,10 @@
+/*
+ * This file is part of TI. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution and at http://creativecommons.org/publicdomain/zero/1.0/. No part of TI,
+ * including this file, may be copied, modified, propagated, or distributed except according to the terms contained
+ * in the LICENSE file.
+ */
+
 /**
  * Logging handlers handle POI invocations by using converters to create a logging message and finally sending the
  * logging message into a logging sink.
